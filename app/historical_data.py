654bf5f9640bda108f77f53cbc321fa0fc131d3c
ajayakumar123 <34769475+ajayakumar123@users.noreply.github.com>
2024-05-25 07:10:56 +0530

    002-sample historical data added

---
+++ app/historical_data.py
@@ -0,0 +1,21 @@
+import json
+from pymongo import MongoClient 
+ 
+ 
+# Making Connection
+try:
+    myclient = MongoClient("mongodb://localhost:27017/") 
+    
+    # database 
+    db = myclient["estimation_db"]
+    Collection = db["tasks"]
+
+
+    with open('historical_data.json') as file:
+        data = json.load(file)
+
+    Collection.insert_many(data)
+    
+except Exception as e:
+    print(e)
+
